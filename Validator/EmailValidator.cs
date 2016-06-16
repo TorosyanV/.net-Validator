7e7cdae894cf1d1f9748e40c2950b0bd343d22b6
torosyanv <v.torosyan@outlook.com>
2016-06-16 17:59:34 +0400

    delete unused referances

--- Validator/EmailValidator.cs
+++ Validator/EmailValidator.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Validator
 {
